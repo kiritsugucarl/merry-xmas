2a0ac579774a53095633475294785aaca10d26f8
kiritsugucarl <cdbpineda02@gmail.com>
2025-12-24 05:37:02 +0800

    fix stats_p1

--- src/sections/Stats/stats_p1.jsx
+++ src/sections/Stats/stats_p1.jsx
@@ -5,17 +5,14 @@ const stats_p1 = () => {
         <div className="container">
             <h1 className="container-header">Overall 2025 Stats</h1>
             <div className="container-entry">
-                <p className="entry">1 out of town together</p>
                 <p className="entry">1 concert attended together</p>
                 <p className="entry">2 movies together</p>
-                <p className="entry">4 months of being together</p>
                 <p className="entry">5 photobooth entries (oldmoon most visited)</p>
                 <p className="entry">7 cafes visited (Starbucks most visited)</p>
                 <p className="entry">8 malls visited (SM Megamall most visited)</p>
                 <p className="entry">17 dates together</p>
                 <p className="entry">223 photos together (on my phone)</p>
                 <p className="entry">1hr 41mins 55secs longest phone duration</p>
-                <p className="entry">Uncountable kisses</p>
             </div>
         </div>
     )     
